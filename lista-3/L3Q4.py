019f8fb94f58d877d95b9b4488457d15b8a831b1
PalomaGLima <palomalima272@gmail.com>
2023-08-22 17:52:49 -0300

    08

--- lista-3/L3Q4.py
+++ lista-3/L3Q4.py
@@ -1,18 +1,20 @@
 L = [8, 21, 2, 9, 16, 12, 17, 19, 6, 10]
+N = len(L)
+maior = 0
+posma = 0
 
-maior_impar = float('-inf')
-pos_maior_impar = -1
+for i in range(0, N, 2):
+    if (L[i] > maior):
+        maior = L[i]
+        posma = i
+print(maior)
 
-# Encontra o maior elemento em posição ímpar e sua posição
-for i in range(1, len(L), 2):
-    if L[i] > maior_impar:
-        maior_impar = L[i]
-        pos_maior_impar = i
+if (N % 2 == 0):
+    posim = N - 1
 
-if pos_maior_impar != -1:
-    # Move o maior elemento ímpar para a última posição ímpar
-    ultimo_impar = len(L) - 1
-    L[pos_maior_impar], L[ultimo_impar] = L[ultimo_impar], L[pos_maior_impar]
+else:
+    posim = N - 1
+L[posma] = L[posim]
+L[posim] = maior
 
 print(L)
-# errada
